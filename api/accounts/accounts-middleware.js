cd5c84d54856542f68097359653c5320bd8e4612
Evan <evan.dieterich.az@gmail.com>
2022-02-13 10:57:50 -0700

    server, get, and getById + middleware complete

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,30 @@
+const Account = require('./accounts-model')
+
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
+  next()
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountNameUnique = async (req, res, next) => {
+  const accountName = await Account.getAll()
+  if (account in accountName)
+  next()
 }
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountId = async (req, res, next) => {
+  try {
+    const account = await Account.getById(req.params.id)
+    if(!account) {
+      next({status: 404, Message: 'Account not found'})
+    }
+    else {
+      req.account = account;
+      next()
+    }
+  }
+  catch (err){
+    next(err)
+  }
 }
